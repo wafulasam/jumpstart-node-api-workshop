a7c8565419986f976b72d2d1c35ee1a7947f71cc
wafulasam <wafulasammie@gmail.com>
2022-01-02 23:30:50 +0300

    - add cors

--- app.ts
+++ app.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express(); // create instance of express app
 const port = process.env.port || 3000; // define port
+
+app.use(cors);
 
 // default home route
 app.get('/', (req: Request, res: Response) => res.send('Welcome to jumpstart api Data'));
